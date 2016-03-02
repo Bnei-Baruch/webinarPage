357bbf4c3eed7e56f70710c4fd7eb34928a4d221
davgur <davgur.ru@gmail.com>
2016-03-02 18:19:31 +0200

    translate mailchimp form error messages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -40,10 +40,11 @@
 (function(app) {
     app.service('GetDataSVC', GetDataSVC);
 
-    function GetDataSVC() {
+    function GetDataSVC($timeout) {
         return {
             getFooterData: getFooterData,
-            getTopNav: getTopNav
+            getTopNav: getTopNav,
+            getMailChimpTranslation: getMailChimpTranslation
         };
 
         function getFooterData() {
@@ -137,7 +138,33 @@
                 "title": "магазин книг"
             }];
         }
+
+
+        /*** get object of messases translation */
+        function getMailChimpTranslation() {
+            return {
+                required: "Это поле необходимо заполнить.",
+                remote: "Пожалуйста, введите правильное значение.",
+                email: "Пожалуйста, введите корректный адрес электронной почты.",
+                url: "Пожалуйста, введите корректный URL.",
+                date: "Пожалуйста, введите корректную дату.",
+                dateISO: "Пожалуйста, введите корректную дату в формате ISO.",
+                number: "Пожалуйста, введите число.",
+                digits: "Пожалуйста, вводите только цифры.",
+                creditcard: "Пожалуйста, введите правильный номер кредитной карты.",
+                equalTo: "Пожалуйста, введите такое же значение ещё раз.",
+                accept: "Пожалуйста, выберите файл с правильным расширением.",
+                maxlength: jQuery.validator.format("Пожалуйста, введите не больше {0} символов."),
+                minlength: jQuery.validator.format("Пожалуйста, введите не меньше {0} символов."),
+                rangelength: jQuery.validator.format("Пожалуйста, введите значение длиной от {0} до {1} символов."),
+                range: jQuery.validator.format("Пожалуйста, введите число от {0} до {1}."),
+                max: jQuery.validator.format("Пожалуйста, введите число, меньшее или равное {0}."),
+                min: jQuery.validator.format("Пожалуйста, введите число, большее или равное {0}.")
+            };
+        }
     }
+
+
 })(angular.module('bbWebinar'));
 
 (function(app) {
@@ -343,12 +370,23 @@
 
 (function (app) {
 	app.controller('FooterCtrl', Controller);
-	Controller.$ingect = ["GetDataSVC"];
+	Controller.$ingect = ["GetDataSVC", '$timeout'];
 	
-	function Controller (GetDataSVC) {
+	function Controller (GetDataSVC, $timeout) {
 		var vm = this;
 		vm.navData = GetDataSVC.getFooterData();
+	 	setMChTranslation();
 		return vm;
+		
+		function setMChTranslation (argument) {
+	        if (!jQuery.validator) {
+	            $timeout(setMChTranslation, 0, false);
+	            return;
+	        }
+	        
+			var msgObj = GetDataSVC.getMailChimpTranslation();		
+            angular.extend(jQuery.validator.messages, msgObj);
+		}
 	}
 
 }(angular.module('bbWebinar')));
@@ -450,26 +488,6 @@
     ftypes[1] = 'text';
     fnames[3] = 'MMERGE3';
     ftypes[3] = 'text';
-
-    jQuery.extend(jQuery.validator.messages, {
-        required: "Это поле необходимо заполнить.",
-        remote: "Пожалуйста, введите правильное значение.",
-        email: "Пожалуйста, введите корректный адрес электронной почты.",
-        url: "Пожалуйста, введите корректный URL.",
-        date: "Пожалуйста, введите корректную дату.",
-        dateISO: "Пожалуйста, введите корректную дату в формате ISO.",
-        number: "Пожалуйста, введите число.",
-        digits: "Пожалуйста, вводите только цифры.",
-        creditcard: "Пожалуйста, введите правильный номер кредитной карты.",
-        equalTo: "Пожалуйста, введите такое же значение ещё раз.",
-        accept: "Пожалуйста, выберите файл с правильным расширением.",
-        maxlength: jQuery.validator.format("Пожалуйста, введите не больше {0} символов."),
-        minlength: jQuery.validator.format("Пожалуйста, введите не меньше {0} символов."),
-        rangelength: jQuery.validator.format("Пожалуйста, введите значение длиной от {0} до {1} символов."),
-        range: jQuery.validator.format("Пожалуйста, введите число от {0} до {1}."),
-        max: jQuery.validator.format("Пожалуйста, введите число, меньшее или равное {0}."),
-        min: jQuery.validator.format("Пожалуйста, введите число, большее или равное {0}.")
-    });
 })(jQuery);
 
 	
--- src/controllers/FooterCtrl.js
+++ src/controllers/FooterCtrl.js
@@ -1,11 +1,22 @@
 (function (app) {
 	app.controller('FooterCtrl', Controller);
-	Controller.$ingect = ["GetDataSVC"];
+	Controller.$ingect = ["GetDataSVC", '$timeout'];
 	
-	function Controller (GetDataSVC) {
+	function Controller (GetDataSVC, $timeout) {
 		var vm = this;
 		vm.navData = GetDataSVC.getFooterData();
+	 	setMChTranslation();
 		return vm;
+		
+		function setMChTranslation (argument) {
+	        if (!jQuery.validator) {
+	            $timeout(setMChTranslation, 0, false);
+	            return;
+	        }
+	        
+			var msgObj = GetDataSVC.getMailChimpTranslation();		
+            angular.extend(jQuery.validator.messages, msgObj);
+		}
 	}
 
 }(angular.module('bbWebinar')));
